1eb0b0c4ed29699905f0662c4bec430b3f615ea6
Nishant5454 <bhagatnishant5454@gmail.com>
2025-01-20 16:39:56 +0530

    All problem solved for the Day3

---
+++ HospitalManagment.java
@@ -0,0 +1,17 @@
+public class HospitalManagment {
+    
+    
+}
+/*
+ * Create a Patient class with the following features:
+Static:
+A static variable hospitalName shared among all patients.
+A static method getTotalPatients() to count the total patients admitted.
+This:
+Use this to initialize name, age, and ailment in the constructor.
+Final:
+Use a final variable patientID to uniquely identify each patient.
+Instanceof:
+Check if an object is an instance of the Patient class before displaying its details.
+
+ */
